82f920c7dc5afc76fe1d0993be09ecd5b01808ba
GUSTAVO RIBEIRO DOS SANTOS BRITO <gustavo.rsbrito@senacsp.edu.br>
2023-09-29 22:00:35 -0300

    Criação da rota de cadastro e conexão com bd

---
+++ src/main/java/br/com/carstore/servlet/CreateCarServlet.java
@@ -0,0 +1,35 @@
+package br.com.carstore.servlet;
+
+import br.com.carstore.dao.CarDao;
+import br.com.carstore.model.Car;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/create-car")
+public class  CreateCarServlet extends HttpServlet  {
+    @Override
+
+    protected void doPost(HttpServletRequest request , HttpServletResponse response) throws ServletException, IOException{
+
+
+        CarDao carDao = new CarDao();
+
+        Car car = new Car();
+
+
+
+        String carName = request.getParameter("car-name");
+        System.out.println(carName);
+
+        car.setName(carName);
+        carDao.createCar(car);
+
+        request.getRequestDispatcher("index.html").forward(request, response);
+    }
+
+}
